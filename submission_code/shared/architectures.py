8f167104e0ee16c00b02c5bdf5a48dd6aa580910
mymakar <maggieymakar@gmail.com>
2021-06-01 16:02:10 -0400

    submission code

---
+++ submission_code/shared/architectures.py
@@ -0,0 +1,154 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library of neural network architectures used."""
+
+
+import tensorflow as tf
+from tensorflow.python.keras import backend
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+def create_architecture(params):
+
+	if params['architecture'] == 'simple':
+		net = SimpleConvolutionNet(
+			dropout_rate=params["dropout_rate"],
+			l2_penalty=params["l2_penalty"],
+			embedding_dim=params["embedding_dim"])
+
+	elif params['architecture'] == 'pretrained_resnet' and (params['random_augmentation'] == "False"):
+		net = PretrainedResNet50(
+			l2_penalty=params["l2_penalty"])
+
+	elif params['architecture'] == 'resnet_random':
+		net = RandomResNet50(
+			l2_penalty=params["l2_penalty"])
+
+	elif (params['architecture'] == 'pretrained_resnet') and (params['random_augmentation'] == "True"):
+		net = PretrainedResNet50_RandomAugmentation(
+			l2_penalty=params["l2_penalty"])
+
+	return net
+
+class PretrainedResNet50(tf.keras.Model):
+	"""Resnet50 pretrained on imagenet."""
+
+	def __init__(self, l2_penalty=0.0, l2_penalty_last_only=False):
+		super(PretrainedResNet50, self).__init__()
+
+		self.resenet = ResNet50(include_top=False, layers=tf.keras.layers,
+			weights='imagenet')
+		self.avg_pool = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')
+
+		if not l2_penalty_last_only:
+			regularizer = tf.keras.regularizers.l2(l2_penalty)
+			for layer in self.resenet.layers:
+				if hasattr(layer, 'kernel'):
+					self.add_loss(lambda layer=layer: regularizer(layer.kernel))
+		self.dense = tf.keras.layers.Dense(1,
+			kernel_regularizer=tf.keras.regularizers.l2(l2_penalty))
+
+	@tf.function
+	def call(self, inputs, training=False):
+		x = self.resenet(inputs, training)
+		x = self.avg_pool(x)
+		return self.dense(x), x
+
+
+class PretrainedResNet50_RandomAugmentation(tf.keras.Model):
+	"""Resnet50 pretrained on imagenet with random augmentations"""
+
+	def __init__(self, l2_penalty=0.0, l2_penalty_last_only=False):
+		super(PretrainedResNet50_RandomAugmentation, self).__init__()
+
+		self.data_augmentation = tf.keras.Sequential([
+			tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+			tf.keras.layers.experimental.preprocessing.RandomRotation(0.2)
+		])
+
+		self.resenet = ResNet50(include_top=False, layers=tf.keras.layers,
+			weights='imagenet')
+		self.avg_pool = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')
+
+		if not l2_penalty_last_only:
+			regularizer = tf.keras.regularizers.l2(l2_penalty)
+			for layer in self.resenet.layers:
+				if hasattr(layer, 'kernel'):
+					self.add_loss(lambda layer=layer: regularizer(layer.kernel))
+		self.dense = tf.keras.layers.Dense(1,
+			kernel_regularizer=tf.keras.regularizers.l2(l2_penalty))
+
+	@tf.function
+	def call(self, inputs, training=False):
+		if training:
+			x = self.data_augmentation(inputs, training=training)
+		else: 
+			x = inputs
+		x = self.resenet(inputs, training)
+		x = self.avg_pool(x)
+		return self.dense(x), x
+
+class RandomResNet50(tf.keras.Model):
+	"""Randomly initialized resnet 50."""
+
+	def __init__(self, l2_penalty=0.0):
+		super(RandomResNet50, self).__init__()
+		self.resenet = ResNet50(include_top=False, layers=tf.keras.layers,
+			weights=None)
+		self.avg_pool = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')
+
+		regularizer = tf.keras.regularizers.l2(l2_penalty)
+		for layer in self.resenet.layers:
+			if hasattr(layer, 'kernel'):
+				self.add_loss(lambda layer=layer: regularizer(layer.kernel))
+		self.dense = tf.keras.layers.Dense(1,
+			kernel_regularizer=tf.keras.regularizers.l2(l2_penalty))
+
+	@tf.function
+	def call(self, inputs, training=False):
+		x = self.resenet(inputs, training)
+		x = self.avg_pool(x)
+		return self.dense(x), x
+
+
+
+class SimpleConvolutionNet(tf.keras.Model):
+	"""Simple architecture with convolutions + max pooling."""
+
+	def __init__(self, dropout_rate=0.0, l2_penalty=0.0, embedding_dim=1000):
+		super(SimpleConvolutionNet, self).__init__()
+		# self.scale = preprocessing.Rescaling(1.0 / 255)
+		self.conv1 = tf.keras.layers.Conv2D(32, [3, 3], activation="relu")
+		self.conv2 = tf.keras.layers.Conv2D(64, [3, 3], activation="relu")
+		self.maxpool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
+		self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+		self.flatten1 = tf.keras.layers.Flatten()
+		self.dense1 = tf.keras.layers.Dense(
+			embedding_dim,
+			activation="relu",
+			kernel_regularizer=tf.keras.regularizers.L2(l2=l2_penalty),
+			name="Z")
+		self.dense2 = tf.keras.layers.Dense(1)
+
+	def call(self, inputs, training=False):
+		z = self.conv1(inputs)
+		z = self.conv2(z)
+		z = self.maxpool1(z)
+		if training:
+			z = self.dropout(z, training=training)
+		z = self.flatten1(z)
+		z = self.dense1(z)
+		return self.dense2(z), z
+
---
+++ submission_code/shared/utils.py
@@ -0,0 +1,96 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Shared utility functions."""
+
+import hashlib
+import os
+
+from subprocess import call
+import tensorflow as tf
+
+
+def restrict_GPU_tf(gpuid, memfrac=0, use_cpu=False):
+	""" Function to pick the gpu to run on
+		Args:
+			gpuid: str, comma separated list "0" or "0,1" or even "0,1,3"
+			memfrac: float, fraction of memory. By default grows dynamically
+	"""
+	if not use_cpu:
+		os.environ["CUDA_VISIBLE_DEVICES"] = gpuid
+
+		config = tf.compat.v1.ConfigProto()
+		if memfrac == 0:
+			config.gpu_options.allow_growth = True
+		else:
+			config.gpu_options.per_process_gpu_memory_fraction = memfrac
+		tf.compat.v1.Session(config=config)
+		print("Using GPU:{} with {:.0f}% of the memory".format(gpuid, memfrac * 100))
+	else:
+		os.environ["CUDA_VISIBLE_DEVICES"] = ""
+		os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+		print("Using CPU")
+
+
+def config_hasher(config):
+	"""Generates hash string for a given config.
+	Args:
+		config: dict with hyperparams ordered by key
+	Returns:
+		hash of config
+	"""
+	config_string = ' '.join('--%s %s' % (k, str(v)) for k, v in config.items())
+	hash_string = hashlib.sha256(config_string.encode()).hexdigest()
+	return hash_string
+
+
+def tried_config(config, base_dir):
+	"""Tests if config has been tried before.
+	Args:
+		config: hyperparam config
+		base_dir: directory where the tuning folder lives
+	"""
+	hash_string = config_hasher(config)
+	hash_dir = os.path.join(base_dir, 'tuning', hash_string)
+	performance_file = os.path.join(hash_dir, 'performance.pkl')
+	# performance_file = os.path.join(hash_dir, 'asym_performance.pkl')
+	return os.path.isfile(performance_file)
+
+
+def tried_config_file(config, base_dir):
+	"""Tests if config has been tried before.
+	Args:
+		config: hyperparam config
+		base_dir: directory where the tuning folder lives
+	"""
+	hash_string = config_hasher(config)
+	hash_dir = os.path.join(base_dir, 'tuning', hash_string)
+	performance_file = os.path.join(hash_dir, 'performance.pkl')
+	# performance_file = os.path.join(hash_dir, 'asym_performance.pkl')
+	if os.path.isfile(performance_file):
+		return config
+
+
+def delete_config_file(config, base_dir):
+	""" deletes results for the specified config.
+		Args:
+		config: hyperparam config
+		base_dir: directory where the tuning folder lives
+	"""
+
+	hash_string = config_hasher(config)
+	hash_dir = os.path.join(base_dir, 'tuning', hash_string)
+	if os.path.exists(hash_dir):
+		call(f'rm -rf {hash_dir}', shell=True)
